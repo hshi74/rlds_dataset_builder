38262b3baa3d1c186dfe99f107858bdf3aa51f75
hshi74 <haochen213300@gmail.com>
2023-08-30 13:45:14 -0700

    Update the data parser

--- robocook/README.md
+++ robocook/README.md
@@ -1,5 +1,19 @@
-TODO(example_dataset): Markdown description of your dataset.
-Description is **formatted** as markdown.
+This is the real-world robot manipulation dataset collected for [RoboCook: Long-Horizon Elasto-Plastic Object Manipulation with Diverse Tools](https://hshi74.github.io/robocook/).
 
-It should also contain any processing which has been applied (if any),
-(e.g. corrupted example skipped, images cropped,...):
+We have collected approximately 20 minutes of interaction data between each tool and the dough using a policy that randomly samples actions from a predefined action space. After each episode, a human reshapes the dough into a different random configuration.
+
+The data breakdown per tool is as follows:
+
+1. Asymmetric gripper / two-rod symmetric gripper / two-plane symmetric gripper: 300 episodes each; 
+1. Circle press / square press / circle punch / square punch: 270 episodes each;
+1. Large roller / small roller: 240 episodes each.
+
+In total, this aggregates to 2460 episodes, with each episode comprising roughly 50 frames.
+
+We use the 7-DoF Franka Emika Panda robot arm and its parallel jaw gripper as the base robot. Four calibrated Intel RealSense D415 RGB-D cameras are fixed on vertical metal bars around the robot tabletop. The cameras capture **1280×720** RGB-D images. We also design a set of 3D-printed tools based on real-world dough manipulation tools. Please refer to the [paper](https://arxiv.org/abs/2306.14447) for more details on our setup.
+
+During the data collection phase, we captured a temporally and spatially smoothed point cloud stream. To optimize storage and maintain compatibility with the RLDS dataset format, we projected the point cloud back to an RGB-D image. Consequently, it's typical to notice gaps in the image, indicative of filtered-out noisy depth values. We also include both camera extrinsics and intrinsics within the dataset, facilitating point cloud reconstruction if needed.
+
+A visualization of the data collection process:
+
+[![RoboCook Data Collection](https://img.youtube.com/vi/VxkOF6mS90I/0.jpg)](https://www.youtube.com/watch?v=VxkOF6mS90I)
--- robocook/robocook_dataset_builder.py
+++ robocook/robocook_dataset_builder.py
@@ -12,20 +12,49 @@ import rosbag
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
-
+from scipy.spatial.transform import Rotation as R
+from transforms3d.euler import quat2euler
 from transforms3d.quaternions import *
 
-
 DEPTH_OPTICAL_FRAME_POSE = [0, 0, 0, 0.5, -0.5, 0.5, -0.5]
+
+with open(
+    os.path.join("/home/haochen/projects/rlds_dataset_builder/cam_pose.json"), "r"
+) as f:
+    cam_pose_dict = json.load(f)
+
+EXTRINSICS = [np.eye(4) for _ in range(4)]
+for i in range(4):
+    EXTRINSICS[i][:3, :3] = quat2mat(
+        cam_pose_dict[f"cam_{i+1}"]["orientation"]
+    ) @ quat2mat(DEPTH_OPTICAL_FRAME_POSE[3:])
+    EXTRINSICS[i][:3, 3] = np.array(cam_pose_dict[f"cam_{i+1}"]["position"])
+    EXTRINSICS[i] = EXTRINSICS[i].astype(np.float32)
+
 INTRINSICS = np.array(
     [
-        [892, 0.0, 660],
-        [0.0, 892, 355],
+        [891.4971313476562, 0.0, 651.0776977539062],
+        [0.0, 891.4971313476562, 357.4739990234375],
         [0.0, 0.0, 1.0],
-    ]
+    ],
+    dtype=np.float32,
 )
 WIDTH = 1280
 HEIGHT = 720
+
+
+def add_angles(delta, source, degrees=False):
+    delta_rot = R.from_euler("xyz", delta, degrees=degrees)
+    source_rot = R.from_euler("xyz", source, degrees=degrees)
+    new_rot = delta_rot * source_rot
+    return new_rot.as_euler("xyz", degrees=degrees)
+
+
+def angle_diff(target, source, degrees=False):
+    target_rot = R.from_euler("xyz", target, degrees=degrees)
+    source_rot = R.from_euler("xyz", source, degrees=degrees)
+    result = target_rot * source_rot.inv()
+    return result.as_euler("xyz")
 
 
 def read_ros_bag(ros_bag_path):
@@ -73,10 +102,14 @@ def parse_pointcloud2(msg):
     return cloud_xyz, cloud_rgb
 
 
-def merge(pc_msgs, extrinsics, visualize=False):
-    points_list = []
-    rgb_list = []
-    depth_list = []
+def process(rosbag_path, visualize=False):
+    pc_msgs, ee_pos, ee_quat, finger_width = read_ros_bag(rosbag_path)
+    state = np.concatenate(
+        [ee_pos, add_angles([np.pi, 0, 0], quat2euler(ee_quat)), [finger_width]]
+    ).astype(np.float32)
+
+    observation = {"state": state}
+    merge_pc = o3d.geometry.PointCloud()
     for i in range(len(pc_msgs)):
         cloud_xyz, cloud_rgb = parse_pointcloud2(pc_msgs[i])
 
@@ -99,44 +132,35 @@ def merge(pc_msgs, extrinsics, visualize=False):
         ).astype(np.uint8)
         # print(rgb.max())
 
-        depth = np.zeros((HEIGHT, WIDTH))
+        depth = np.zeros((HEIGHT, WIDTH), dtype=np.float32)
         depth[pixel_pos[valid_indices, 1], pixel_pos[valid_indices, 0]] = cloud_xyz[
             :, 2
         ][valid_indices]
 
-        depth = np.clip(depth, 0, 2.0) * 1000
+        depth = np.clip(depth, 0.0, 2.0) * 1000.0
         # print(depth.max())
 
-        points = (
-            quat2mat(DEPTH_OPTICAL_FRAME_POSE[3:]) @ cloud_xyz[:, :3].T
-        ).T + DEPTH_OPTICAL_FRAME_POSE[:3]
-        points_transform = points @ extrinsics[i][:3, :3].T + extrinsics[i][:3, 3]
+        # points = cloud_xyz @ EXTRINSICS[i][:3, :3].T + EXTRINSICS[i][:3, 3]
+        # pc = o3d.geometry.PointCloud()
+        # pc.points = o3d.utility.Vector3dVector(points[cloud_xyz[:, 2] < 1.0])
+        # pc.colors = o3d.utility.Vector3dVector(cloud_rgb[cloud_xyz[:, 2] < 1.0])
+        # merge_pc += pc
 
-        depth_filter = cloud_xyz[:, 2] < 1.0
-        points_transform = points_transform[depth_filter]
-        cloud_rgb = cloud_rgb[depth_filter]
-
-        rgb_list.append(rgb)
-        depth_list.append(depth)
-        points_list.append(np.concatenate([points_transform, cloud_rgb], axis=-1))
-
-    merge_points = np.concatenate(points_list, axis=0)
+        observation[f"image_{i+1}"] = rgb
+        observation[f"depth_{i+1}"] = depth
 
     if visualize:
+        o3d.visualization.draw_geometries([merge_pc])
+
         figure, axes = plt.subplots(len(pc_msgs), 2, figsize=(16, 16))
         for i in range(len(pc_msgs)):
-            axes[i][0].imshow(rgb_list[i])
-            axes[i][1].imshow(depth_list[i])
+            axes[i][0].imshow(observation[f"image_{i+1}"])
+            axes[i][1].imshow(observation[f"depth_{i+1}"])
 
         plt.show()
         plt.close()
 
-        pc = o3d.geometry.PointCloud()
-        pc.points = o3d.utility.Vector3dVector(merge_points[:, :3])
-        pc.colors = o3d.utility.Vector3dVector(merge_points[:, 3:])
-        o3d.visualization.draw_geometries([pc])
-
-    return merge_points
+    return observation
 
 
 class Robocook(tfds.core.GeneratorBasedBuilder):
@@ -162,31 +186,63 @@ class Robocook(tfds.core.GeneratorBasedBuilder):
                         {
                             "observation": tfds.features.FeaturesDict(
                                 {
-                                    "image": tfds.features.Image(
-                                        shape=(64, 64, 3),
+                                    "image_1": tfds.features.Image(
+                                        shape=(720, 1280, 3),
                                         dtype=np.uint8,
                                         encoding_format="png",
-                                        doc="Main camera RGB observation.",
+                                        doc="Camera 1 RGB observation.",
                                     ),
-                                    "wrist_image": tfds.features.Image(
-                                        shape=(64, 64, 3),
+                                    "image_2": tfds.features.Image(
+                                        shape=(720, 1280, 3),
                                         dtype=np.uint8,
                                         encoding_format="png",
-                                        doc="Wrist camera RGB observation.",
+                                        doc="Camera 2 RGB observation.",
+                                    ),
+                                    "image_3": tfds.features.Image(
+                                        shape=(720, 1280, 3),
+                                        dtype=np.uint8,
+                                        encoding_format="png",
+                                        doc="Camera 3 RGB observation.",
+                                    ),
+                                    "image_4": tfds.features.Image(
+                                        shape=(720, 1280, 3),
+                                        dtype=np.uint8,
+                                        encoding_format="png",
+                                        doc="Camera 4 RGB observation.",
+                                    ),
+                                    "depth_1": tfds.features.Tensor(
+                                        shape=(720, 1280),
+                                        dtype=np.float32,
+                                        doc="Camera 1 Depth observation.",
+                                    ),
+                                    "depth_2": tfds.features.Tensor(
+                                        shape=(720, 1280),
+                                        dtype=np.float32,
+                                        doc="Camera 2 Depth observation.",
+                                    ),
+                                    "depth_3": tfds.features.Tensor(
+                                        shape=(720, 1280),
+                                        dtype=np.float32,
+                                        doc="Camera 3 Depth observation.",
+                                    ),
+                                    "depth_4": tfds.features.Tensor(
+                                        shape=(720, 1280),
+                                        dtype=np.float32,
+                                        doc="Camera 4 Depth observation.",
                                     ),
                                     "state": tfds.features.Tensor(
-                                        shape=(10,),
+                                        shape=(7,),
                                         dtype=np.float32,
-                                        doc="Robot state, consists of [7x robot joint angles, "
-                                        "2x gripper position, 1x door opening angle].",
+                                        doc="Robot state, consists of [3x robot end-effector position, "
+                                        "3x robot end-effector euler angles, 1x gripper position].",
                                     ),
                                 }
                             ),
                             "action": tfds.features.Tensor(
-                                shape=(10,),
+                                shape=(7,),
                                 dtype=np.float32,
-                                doc="Robot action, consists of [7x joint velocities, "
-                                "2x gripper velocities, 1x terminate episode].",
+                                doc="Robot action, consists of [3x robot end-effector velocities, "
+                                "3x robot end-effector angular velocities, 1x gripper velocity].",
                             ),
                             "discount": tfds.features.Scalar(
                                 dtype=np.float32,
@@ -222,6 +278,46 @@ class Robocook(tfds.core.GeneratorBasedBuilder):
                             "file_path": tfds.features.Text(
                                 doc="Path to the original data file."
                             ),
+                            "intrinsics_1": tfds.features.Tensor(
+                                shape=(3, 3),
+                                dtype=np.float32,
+                                doc="Camera 1 Intrinsic Matrix.",
+                            ),
+                            "intrinsics_2": tfds.features.Tensor(
+                                shape=(3, 3),
+                                dtype=np.float32,
+                                doc="Camera 2 Intrinsic Matrix.",
+                            ),
+                            "intrinsics_3": tfds.features.Tensor(
+                                shape=(3, 3),
+                                dtype=np.float32,
+                                doc="Camera 3 Intrinsic Matrix.",
+                            ),
+                            "intrinsics_4": tfds.features.Tensor(
+                                shape=(3, 3),
+                                dtype=np.float32,
+                                doc="Camera 4 Intrinsic Matrix.",
+                            ),
+                            "extrinsics_1": tfds.features.Tensor(
+                                shape=(4, 4),
+                                dtype=np.float32,
+                                doc="Camera 1 Extrinsic Matrix.",
+                            ),
+                            "extrinsics_2": tfds.features.Tensor(
+                                shape=(4, 4),
+                                dtype=np.float32,
+                                doc="Camera 2 Extrinsic Matrix.",
+                            ),
+                            "extrinsics_3": tfds.features.Tensor(
+                                shape=(4, 4),
+                                dtype=np.float32,
+                                doc="Camera 3 Extrinsic Matrix.",
+                            ),
+                            "extrinsics_4": tfds.features.Tensor(
+                                shape=(4, 4),
+                                dtype=np.float32,
+                                doc="Camera 4 Extrinsic Matrix.",
+                            ),
                         }
                     ),
                 }
@@ -231,22 +327,14 @@ class Robocook(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Define data splits."""
         return {
-            "train": self._generate_examples(path="data/train/episode_*.npy"),
+            "train": self._generate_examples(),
             # "val": self._generate_examples(path="data/val/episode_*.npy"),
         }
 
-    def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
+    def _generate_examples(self) -> Iterator[Tuple[str, Any]]:
         """Generator of examples for each split."""
 
-        with open(os.path.join("cam_pose.json"), "r") as f:
-            cam_pose_dict = json.load(f)
-
-        extrinsics = [np.eye(4) for _ in range(4)]
-        for i in range(4):
-            extrinsics[i][:3, :3] = quat2mat(cam_pose_dict[f"cam_{i+1}"]["orientation"])
-            extrinsics[i][:3, 3] = np.array(cam_pose_dict[f"cam_{i+1}"]["position"])
-
-        def _parse_example(episode_path):
+        def _parse_example(episode_path, language_instruction):
             # load raw data --> this should change for your dataset
             # data = np.load(
             #     episode_path, allow_pickle=True
@@ -254,78 +342,92 @@ class Robocook(tfds.core.GeneratorBasedBuilder):
 
             # for i, step in enumerate(data):
 
-            rosbag_path_list = glob.glob(os.path.join(episode_path, "*.bag"))
+            rosbag_path_list = sorted(glob.glob(os.path.join(episode_path, "*.bag")))
+
+            observation_list = []
+            for i, rosbag_path in enumerate(rosbag_path_list):
+                observation = process(rosbag_path, visualize=False)
+                observation_list.append(observation)
 
             episode = []
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
-            for i, rosbag_path in enumerate(rosbag_path_list):
-                rosbag_path = "/media/haochen/Game Drive PS41/robocook/raw_data/roller_large_robot_v4/ep_000/seq_000/0.000.bag"
-                pc_msgs, ee_pos, ee_quat, finger_width = read_ros_bag(rosbag_path)
+            for i, observation in enumerate(observation_list):
+                action = np.zeros(7, dtype=np.float32)
+                if i < len(observation_list) - 1:
+                    action[:3] = (
+                        observation_list[i + 1]["state"][:3] - observation["state"][:3]
+                    )
+                    action[3:6] = angle_diff(
+                        observation_list[i + 1]["state"][3:6], observation["state"][3:6]
+                    )
+                    action[6] = (
+                        observation_list[i + 1]["state"][6] - observation["state"][6]
+                    )
 
-                step = merge(pc_msgs, extrinsics, visualize=False)
-                # compute Kona language embedding
-                language_embedding = self._embed([step["language_instruction"]])[
-                    0
-                ].numpy()
+                # # compute Kona language embedding
+                language_embedding = self._embed([language_instruction])[0].numpy()
 
                 episode.append(
                     {
-                        "observation": {
-                            "image": step["image"],
-                            "wrist_image": step["wrist_image"],
-                            "state": step["state"],
-                        },
-                        "action": step["action"],
+                        "observation": observation,
+                        "action": action,
                         "discount": 1.0,
-                        "reward": float(i == (len(data) - 1)),
+                        "reward": 0.0,  # float(i == (len(rosbag_path_list) - 1)),
                         "is_first": i == 0,
-                        "is_last": i == (len(data) - 1),
-                        "is_terminal": i == (len(data) - 1),
-                        "language_instruction": step["language_instruction"],
+                        "is_last": i == (len(rosbag_path_list) - 1),
+                        "is_terminal": i == (len(rosbag_path_list) - 1),
+                        "language_instruction": language_instruction,
                         "language_embedding": language_embedding,
                     }
                 )
 
+            metadata = {"file_path": episode_path}
+            for i in range(len(EXTRINSICS)):
+                metadata[f"intrinsics_{i+1}"] = INTRINSICS
+                metadata[f"extrinsics_{i+1}"] = EXTRINSICS[i]
+
             # create output data sample
-            sample = {"steps": episode, "episode_metadata": {"file_path": episode_path}}
+            sample = {"steps": episode, "episode_metadata": metadata}
 
             # if you want to skip an example for whatever reason, simply return None
             return episode_path, sample
 
         # create list of all examples
-        ps4_drive_prefix = "/media/haochen/Game Drive PS4/robocook/raw_data"
-        ps4_drive_task_list = [
-            "gripper_sym_rod_robot_v4",
-            "gripper_sym_plane_robot_v4",
-            "gripper_asym_robot_v4",
-            "roller_large_robot_v4",
-            "roller_small_robot_v4",
-        ]
+        ps4_drive_prefix = "/media/haochen/Game Drive PS41/robocook/raw_data"
+        ps4_drive_tasks = {
+            "gripper_sym_rod_robot_v4": "pinch the dough with a two-rod symmetric gripper",
+            # "gripper_sym_plane_robot_v4": "pinch the dough with a two-plane symmetric gripper",
+            # "gripper_asym_robot_v4": "pinch the dough with an asymmetric gripper",
+            # "roller_large_robot_v4": "roll the dough with a large rolling pin",
+            # "roller_small_robot_v4": "roll the dough with a small rolling pin",
+        }
 
         wd_drive_prefix = "/media/haochen/wd_drive/robocook/raw_data"
-        wd_drive_task_list = [
-            "press_circle_robot_v4",
-            "press_square_robot_v4",
-            "punch_circle_robot_v4",
-            "punch_square_robot_v4",
-        ]
+        wd_drive_tasks = {
+            # "press_circle_robot_v4": "press the dough with a circle press",
+            # "press_square_robot_v4": "press the dough with a square press",
+            # "punch_circle_robot_v4": "press the dough with a circle punch",
+            # "punch_square_robot_v4": "press the dough with a square punch",
+        }
 
         episode_paths = []
-        for ps4_task in ps4_drive_task_list:
+        instructions = []
+        for ps4_task, instruction in ps4_drive_tasks.items():
             path = os.path.join(ps4_drive_prefix, ps4_task, "ep_*/seq_*")
-            episode_paths += glob.glob(path)
+            path_list = sorted(glob.glob(path))
+            episode_paths += path_list
+            instructions += [instruction] * len(path_list)
 
-        for wd_task in wd_drive_task_list:
+        for wd_task, instruction in wd_drive_tasks.items():
             path = os.path.join(wd_drive_prefix, wd_task, "ep_*/seq_*")
-            episode_paths += glob.glob(path)
+            path_list = sorted(glob.glob(path))
+            episode_paths += path_list
+            instructions += [instruction] * len(path_list)
 
-        # for smallish datasets, use single-thread parsing
-        for sample in episode_paths:
-            yield _parse_example(sample)
+        # # for smallish datasets, use single-thread parsing
+        # for sample, instruction in zip(episode_paths, instructions):
+        #     yield _parse_example(sample, instruction)
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
-        # beam = tfds.core.lazy_imports.apache_beam
-        # return (
-        #         beam.Create(episode_paths)
-        #         | beam.Map(_parse_example)
-        # )
+        beam = tfds.core.lazy_imports.apache_beam
+        return beam.Create(zip(episode_paths, instructions)) | beam.Map(_parse_example)
